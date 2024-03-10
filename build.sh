c20b51aed41c9b4a44dd1de9fa4711c6e498f7b2
sayad-mallllek <bob@khalilelzein.com>
2024-03-10 16:14:20 +0200

    remove python3.9 to python

--- build.sh
+++ build.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 # Update pip
-echo "Updating pip..."
-python3.9 pip install -U pip
+# echo "Updating pip..."
+# python3.9 -m pip install -U pip
 
 # Install dependencies
 
 echo "Installing project dependencies..."
-python3.9 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # Make migrations
 echo "Making migrations..."
-python3.9 manage.py makemigrations --noinput
-python3.9 manage.py migrate --noinput
+python manage.py makemigrations --noinput
+python manage.py migrate --noinput
 
 # Collect staticfiles
 echo "Collect static..."
-python3.9 manage.py collectstatic --noinput --clear
+python manage.py collectstatic --noinput --clear
 
 echo "Build process completed!"
