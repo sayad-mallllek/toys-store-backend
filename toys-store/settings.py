44b8b0d53e20407ed3c0597629bc21288425095e
sayad-mallllek <bob@khalilelzein.com>
2024-03-10 15:32:55 +0200

    test 5

--- toys-store/settings.py
+++ toys-store/settings.py
@@ -127,9 +127,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR + "/staticfiles" + "/static"
-
 STATIC_URL = "/static/"
 
 STATICFILES_DIRS = [
