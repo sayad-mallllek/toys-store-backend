345e0ddf170f3133728a774b07dfb3152c2a251b
sayad-mallllek <bob@khalilelzein.com>
2024-03-10 16:18:46 +0200

    test without makemigrations

--- build.sh
+++ build.sh
@@ -10,9 +10,9 @@ echo "Installing project dependencies..."
 python -m pip install -r requirements.txt
 
 # Make migrations
-echo "Making migrations..."
-python manage.py makemigrations --noinput
-python manage.py migrate --noinput
+# echo "Making migrations..."
+# python manage.py makemigrations --noinput
+# python manage.py migrate --noinput
 
 # Collect staticfiles
 echo "Collect static..."
