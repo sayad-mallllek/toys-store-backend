5a08952620dd9cc8ba9db1cc69bb0ba680b79c2b
sayad-mallllek <bob@khalilelzein.com>
2024-03-10 12:21:17 +0200

    fix: static root

--- toys-store/settings.py
+++ toys-store/settings.py
@@ -126,8 +126,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-# STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR + "/staticfiles" + "/static"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
